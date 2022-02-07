e52d1559808fa5e283c9181c3d95a2fa6d026eac
Dan Bertrand <dan.bertrand@azeti.net>
2022-02-07 15:58:54 +0100

    Add sdk PnP yarn for VScode

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import Typography from "@mui/material/Typography";
+import dayjs from "dayjs";
 
 function App() {
+  const date = dayjs("2019-01-25").toString();
+  console.log("Date", date);
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <a>asd</a>
+        <Typography>Hello world </Typography>
+        <span>{date}</span>
       </header>
     </div>
   );
